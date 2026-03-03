b8d0c381ea10f020bf08b1b74dbde33c2334dc34
Miguel Angel <angelfalconmv@gmail.com>
2026-03-03 00:35:46 -0300

    agregando apartado de iformacion, contacto y carro de compras

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const maxFrames = 169;
+
+  const infoModal = document.getElementById('infoModal');
+
+  infoModal.addEventListener('show.bs.modal', (event) => {
+    const trigger = event.relatedTarget; // el <a> que lo abrió
+
+    const title = trigger.getAttribute('data-title');
+    const body = trigger.getAttribute('data-body');
+
+    infoModal.querySelector('#infoModalLabel').textContent = title;
+    infoModal.querySelector('#infoModalBody').innerHTML = body; // innerHTML para que acepte <br>
+  });
